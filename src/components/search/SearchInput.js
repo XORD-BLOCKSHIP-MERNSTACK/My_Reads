fd12e56e3b8819898706ef2b67ecb26ba513a2af
Ayesha Ghani <60398800+ayesha-ghani098@users.noreply.github.com>
2021-08-18 20:22:09 +0500

    This commit add searchInput component

--- src/components/search/SearchInput.js
+++ src/components/search/SearchInput.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchInput = () => {
-  return <div></div>;
+  const [value, setValue] = useState('');
+
+  const handleSearch = () => {};
+
+  return (
+    <div className='search-books-input-wrapper'>
+      <input
+        type='text'
+        value={value}
+        onChange={handleSearch}
+        placeholder='Search by title or author'
+        autoFocus
+      />
+    </div>
+  );
 };
 
 export default SearchInput;
